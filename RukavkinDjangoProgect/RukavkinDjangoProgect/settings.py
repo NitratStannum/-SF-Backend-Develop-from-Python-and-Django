c32a1bd9c3bc1ed512ec0d5d339190df9181775f
NitratStannum <van-der-vals@yandex.ru>
2023-12-17 00:59:52 +0300

    add static styles, add new_page, change settings

--- RukavkinDjangoProgect/RukavkinDjangoProgect/settings.py
+++ RukavkinDjangoProgect/RukavkinDjangoProgect/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
 
     'django.contrib.sites',
     'django.contrib.flatpages',
+
+    'fpages',
 ]
 
 SITE_ID = 1
@@ -128,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
